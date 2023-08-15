f05392c837dfe4b352bb8e4505356c2427703355
dylen400mh <dylenbelanger2004@gmail.com>
2023-08-15 18:23:17 -0400

    Change empty board spots to null, change filled board spots to respective ship object

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,11 +1,13 @@
+import Ship from "./Ship";
+
 const Gameboard = () => {
-  // Creates board. Spots that haven't been checked are false.
+  // Creates board. Spots that haven't been checked are null.
   const createBoard = () => {
     const brd = [];
     for (let i = 0; i < 10; i += 1) {
       const row = [];
       for (let col = 0; col < 10; col += 1) {
-        row[col] = false;
+        row[col] = null;
       }
       brd.push(row);
     }
@@ -21,17 +23,19 @@ const Gameboard = () => {
 
   // Places a ship at specified coordinates and on a specified axis (for now we won't verify the position here)
   const placeShip = (length, row, col, axis) => {
+    const ship = Ship(length);
+
     // if on horizontal axis
     if (axis === "horizontal") {
       for (let i = col, j = 0; j < length; i += 1, j += 1) {
-        board[row][i] = true;
+        board[row][i] = ship;
       }
     }
 
     // if on vertical axis
     if (axis === "vertical") {
       for (let i = row, j = 0; j < length; i += 1, j += 1) {
-        board[i][col] = true;
+        board[i][col] = ship;
       }
     }
   };
