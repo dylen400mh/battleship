73c9592cddbbe8a0ac05698f67d6f11f726d5164
dylen400mh <dylenbelanger2004@gmail.com>
2023-08-19 15:46:09 -0400

    Add method to Ship factory to get cells a ship occupies

--- src/Ship.js
+++ src/Ship.js
@@ -2,6 +2,23 @@ const Ship = (length, row, col, axis) => {
   // hits start at 0
   let hits = 0;
 
+  const getCells = () => {
+    const cells = [];
+    if (axis === "horizontal") {
+      for (let i = col, j = 0; j < length; i += 1, j += 1) {
+        cells.push([row, i]);
+      }
+    }
+
+    if (axis === "vertical") {
+      for (let i = row, j = 0; j < length; i += 1, j += 1) {
+        cells.push([i, col]);
+      }
+    }
+
+    return cells;
+  };
+
   // increments number of hits on a ship
   const hit = () => {
     hits += 1;
@@ -13,7 +30,7 @@ const Ship = (length, row, col, axis) => {
   // determines if a ship has sunk
   const isSunk = () => length === hits;
 
-  return { length, hit, getHits, isSunk, row, col, axis };
+  return { length, hit, getHits, isSunk, row, col, axis, getCells };
 };
 
 export default Ship;
