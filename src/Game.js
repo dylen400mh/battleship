d8a9f7d8d0f8c2381298bc1002bbf438746584e6
dylen400mh <dylenbelanger2004@gmail.com>
2023-08-20 13:34:48 -0400

    Remove comments at bottom of file

--- src/Game.js
+++ src/Game.js
@@ -86,8 +86,3 @@ const Game = (() => {
 })();
 
 export default Game;
-
-// factory function that has state and functions.
-// DOM needs to call from your game object to start the game, what game object would do when a cell is clicked in the UI.
-// make action and retrieve states
-// Update board states
